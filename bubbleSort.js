f03918cfc8ec432586b67ab8dd72c1b1f04ab139
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2024-05-21 09:01:27 +0530

    BubbleSort : do while is optimised when compare to for loop

--- bubbleSort.js
+++ bubbleSort.js
@@ -11,17 +11,19 @@ function bubbleSort(arr) {
   console.log("Before swap :::: ", arr);
   let counter = 0;
   let counter2 = 0;
-  for (let i = 0; i < arr.length; i++) {
+  do {
+    swapped = false;
     counter++;
     for (let j = 0; j < arr.length - 1; j++) {
       counter2++;
       if (arr[j] > arr[j + 1]) {
+        swapped = true;
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
       }
     }
-  }
+  } while (swapped);
   console.log(
     "After swap :::: ",
     arr,
@@ -40,9 +42,10 @@ bubbleSort([3, 4, 2, 1]);
 Before swap ::::  [ 2, 3, 4, 1 ]
 After swap ::::  [ 1, 2, 3, 4 ] counter :::: 4 counter2 :::: 12
 Before swap ::::  [ 3, 1, 2, 4 ]
-After swap ::::  [ 1, 2, 3, 4 ] counter :::: 4 counter2 :::: 12
+After swap ::::  [ 1, 2, 3, 4 ] counter :::: 2 counter2 :::: 6
 Before swap ::::  [ 1, 2, 3, 4 ]
-After swap ::::  [ 1, 2, 3, 4 ] counter :::: 4 counter2 :::: 12
+After swap ::::  [ 1, 2, 3, 4 ] counter :::: 1 counter2 :::: 3
 Before swap ::::  [ 3, 4, 2, 1 ]
 After swap ::::  [ 1, 2, 3, 4 ] counter :::: 4 counter2 :::: 12
+
 */
